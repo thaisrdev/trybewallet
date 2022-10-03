16b73ed03ad465dbfa27ab7bae832843ccfa7568
Thais Rosa <tsrosa18@gmail.com>
2022-10-03 08:47:42 -0300

    tentativa requisito 6

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,61 @@
 import React, { Component } from 'react';
+// import getCurrencies from './api';
 
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <div>
+        <label htmlFor="value-input">
+          {' '}
+          Valor da despesa:
+          <input
+            data-testid="value-input"
+            type="text"
+          />
+        </label>
+
+        <label htmlFor="description-input">
+          {' '}
+          Descrição:
+          <input
+            data-testid="description-input"
+            type="text"
+          />
+        </label>
+
+        <label htmlFor="currency-input">
+          <select data-testid="currency-input">
+            {/* Um campo para selecionar em qual moeda será registrada a despesa.
+            As options devem ser preenchidas pelo valor da chave currencies do estado global.
+            Os valores da chave currencies no estado global devem ser puxados através de uma requisição à API no endpoint https://economia.awesomeapi.com.br/json/all;
+            Remova, das informações trazidas pela API, a opção 'USDT';
+            A chave currencies do estado global deve ser um array. */}
+          </select>
+        </label>
+
+        <label htmlFor="method-input">
+          {' '}
+          Método de pagamento:
+          <select data-testid="method-input">
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+          </select>
+        </label>
+
+        <label htmlFor="tag-input">
+          {' '}
+          Categoria:
+          <select data-testid="tag-input">
+            <option value="Alimentação">Alimentação</option>
+            <option value="Lazer">Lazer</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Saúde">Saúde</option>
+          </select>
+        </label>
+
+      </div>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,8 +3,34 @@ import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div><Header /></div>;
+    return (
+      <div>
+        <Header />
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          {/* <tr>
+    <td></td>
+  </tr> */}
+        </table>
+      </div>
+    );
   }
 }
 
 export default Wallet;
+
+// The <table> tag defines an HTML table.
+
+// An HTML table consists of one <table> element and one or more <tr>, <th>, and <td> elements.
+
+// The <tr> element defines a table row, the <th> element defines a table header, and the <td> element defines a table cell.
